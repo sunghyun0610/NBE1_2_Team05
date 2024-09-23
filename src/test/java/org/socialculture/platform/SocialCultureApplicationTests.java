77f083903f0f41a981147e1087d76cf3bcc961a2
Yechan Jung <skfk286@hanmail.net>
2024-09-23 23:24:16 +0900

    프로젝트 최초 커밋

---
+++ src/test/java/org/socialculture/platform/SocialCultureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.socialculture.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialCultureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
