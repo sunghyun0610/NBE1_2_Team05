a8f77763222a013edca65cd9a36ade4a12e72a92
sunghyun <20190171@inu.ac.kr>
2024-09-26 16:57:20 +0900

    feat : 공통 ApiResponse 성공시까지 구현 및 테스트 해볼것

---
+++ src/main/java/org/socialculture/platform/global/apiResponse/ApiResponse.java
@@ -0,0 +1,53 @@
+package org.socialculture.platform.global.apiResponse;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class ApiResponse<T> {
+
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    private final String code;
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final T result;
+
+    // 생성자
+    public ApiResponse(Boolean isSuccess, String code, String message, T result) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+        this.result = result;
+    }
+
+    // Getter 메서드
+    public Boolean getIsSuccess() {
+        return isSuccess;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    // 성공한 경우 응답 생성
+    public static <T> ApiResponse<T> onSuccess(T result) {
+        return new ApiResponse<>(true, SuccessStatus._OK.getCode(), SuccessStatus._OK.getMessage(), result);
+    }
+
+    // 실패한 경우 응답 생성
+    public static <T> ApiResponse<T> onFailure(String code, String message, T data) {
+        return new ApiResponse<>(false, code, message, data);
+    }
+
+}
