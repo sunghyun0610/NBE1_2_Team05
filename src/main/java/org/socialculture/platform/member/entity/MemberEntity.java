7321e29929642d162da7e08af1f148f7b66e5085
yechan <rladPcks980@gmail.com>
2024-09-27 21:36:02 +0900

    fix: MemberEntity import 변경

--- src/main/java/org/socialculture/platform/member/entity/MemberEntity.java
+++ src/main/java/org/socialculture/platform/member/entity/MemberEntity.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.socialculture.platform.global.entity.BaseEntity;
-import org.socialculture.platform.member.MemberRole;
-import org.socialculture.platform.member.SocialProvider;
 
 @Entity
 @Table(name = "member")
