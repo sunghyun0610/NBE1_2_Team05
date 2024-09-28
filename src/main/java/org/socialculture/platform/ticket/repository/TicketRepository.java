c569df6f97d027b9a2b0bfde24f3eeadd34ea28d
Yechan Jung <skfk286@hanmail.net>
2024-09-28 23:54:42 +0900

    refactor : MemberId 에 대한 전체조회, 상세조회로 수정

--- src/main/java/org/socialculture/platform/ticket/repository/TicketRepository.java
+++ src/main/java/org/socialculture/platform/ticket/repository/TicketRepository.java
@@ -3,10 +3,16 @@ package org.socialculture.platform.ticket.repository;
 import org.socialculture.platform.ticket.entity.TicketEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 /**
  * 티켓 레파지토리
  * 
  * @author ycjung
  */
 public interface TicketRepository extends JpaRepository<TicketEntity, Long> {
+
+    List<TicketEntity> findAllByMemberMemberId(Long memberId);
+    Optional<TicketEntity> findByMemberMemberIdAndTicketId(Long memberId, Long ticketId);
 }
