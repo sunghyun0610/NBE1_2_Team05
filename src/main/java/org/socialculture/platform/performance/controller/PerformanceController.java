f0615d17dd6ab0a2fa54a0f2809c6a004ac2c216
icecoff22 <icecoff2222@gmail.com>
2024-09-30 02:35:25 +0900

    refactor : api endpoint 변경

--- src/main/java/org/socialculture/platform/performance/controller/PerformanceController.java
+++ src/main/java/org/socialculture/platform/performance/controller/PerformanceController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/performance")
+@RequestMapping("/api/v1/performances")
 public class PerformanceController {
 
     private final PerformanceService performanceService;
