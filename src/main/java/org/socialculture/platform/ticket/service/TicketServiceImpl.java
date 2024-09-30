c0a52ab6d7f6ef6408f0fe4cd54acd4beb59d950
Yechan Jung <skfk286@hanmail.net>
2024-09-30 12:05:22 +0900

    refactor : QueryDSL 추가에 따른 코드 수정

--- src/main/java/org/socialculture/platform/ticket/service/TicketServiceImpl.java
+++ src/main/java/org/socialculture/platform/ticket/service/TicketServiceImpl.java
@@ -1,13 +1,16 @@
 package org.socialculture.platform.ticket.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.socialculture.platform.global.apiResponse.exception.ErrorStatus;
 import org.socialculture.platform.global.apiResponse.exception.GeneralException;
 import org.socialculture.platform.member.repository.MemberRepository;
 import org.socialculture.platform.ticket.dto.response.TicketResponseDto;
+import org.socialculture.platform.ticket.entity.MemberEntity;
 import org.socialculture.platform.ticket.entity.TicketEntity;
 import org.socialculture.platform.ticket.repository.TicketRepository;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,6 +22,7 @@ import java.util.stream.Collectors;
  * @author ycjung
  */
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class TicketServiceImpl implements TicketService {
 
@@ -28,16 +32,10 @@ public class TicketServiceImpl implements TicketService {
 
     private static Long MEMBER_ID = 1L;
 
-    public TicketServiceImpl(TicketRepository ticketRepository, MemberRepository memberRepository) {
-        this.ticketRepository = ticketRepository;
-        this.memberRepository = memberRepository;
-    }
-
     @Override
     public List<TicketResponseDto> getAllTicketsByMemberId() {
         try {
-            // 데이터베이스에서 모든 티켓을 조회하고, TicketResponse로 변환
-            return ticketRepository.findAllByMemberMemberId(MEMBER_ID).stream()
+            return ticketRepository.getAllTicketsByMemberId(MEMBER_ID).stream()
                     .map(TicketResponseDto::from)
                     .collect(Collectors.toList());
         } catch (NullPointerException e) {
@@ -53,11 +51,42 @@ public class TicketServiceImpl implements TicketService {
     }
 
     @Override
+    public List<TicketResponseDto> getAllTicketsByMemberIdWithPage(int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+
+        try {
+            return ticketRepository.getAllTicketsByMemberIdWithPage(MEMBER_ID, pageRequest.getOffset(), pageRequest.getPageSize())
+                    .stream()
+                    .map(TicketResponseDto::from)
+                    .collect(Collectors.toList());
+        } catch (NullPointerException e) {
+            log.error("티켓 데이터 변환 문제 발생 : {}", e.getMessage(), e);
+            throw new GeneralException(ErrorStatus._INTERNAL_SERVER_ERROR);
+        } catch (DataAccessException e) {
+            log.error("데이터베이스 오류 발생: {}", e.getMessage(), e);
+            throw new GeneralException(ErrorStatus._INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            log.error("예상치 못한 오류 발생: {}", e.getMessage(), e);
+            throw new GeneralException(ErrorStatus._INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    public List<TicketResponseDto> getAllTicketsByMemberIdWithPageAndSortOptionDesc(int page, int size, String sortOption) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+
+        return ticketRepository.getAllTicketsByMemberIdWithPageAndSortOptionDesc(MEMBER_ID, pageRequest.getOffset(),
+                        pageRequest.getPageSize(), sortOption)
+                .stream()
+                .map(TicketResponseDto::from)
+                .collect(Collectors.toList());
+    }
+
+    @Override
     public TicketResponseDto getTicketByMemberIdAndTicketId(Long ticketId) {
         TicketEntity ticketEntity = ticketRepository.findByMemberMemberIdAndTicketId(MEMBER_ID, ticketId)
                 .orElseThrow(() -> new GeneralException(ErrorStatus._TICKET_NOT_FOUND));
 
         return TicketResponseDto.from(ticketEntity);
     }
-
 }
