33f730d6174466696536aa03b44b0b69b35d76c3
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-09-30 15:22:13 +0900

    feat: jwt 액세스 토큰, 리프레시 토큰 생성

---
+++ src/main/java/org/socialculture/platform/member/oauth/jwt/JwtTokenProvider.java
@@ -0,0 +1,51 @@
+package org.socialculture.platform.member.oauth.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * JWT 토큰 관리 클래스
+ * @author 김연수
+ */
+@Service
+public class JwtTokenProvider {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.access.token.expiration}")
+    private long accessTokenValidity; //액세스 토큰 유효 시간
+
+    @Value("${jwt.refresh.token.expiration}")
+    private long refreshTokenValidity; //리프레시 토큰 유효 시간
+
+    // 액세스 토큰 생성
+    public String createAccessToken(String email) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("email", email);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + accessTokenValidity))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    // 리프레시 토큰 생성
+    public String createRefreshToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenValidity))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+}
+
