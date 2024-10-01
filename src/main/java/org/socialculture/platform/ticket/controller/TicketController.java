8bbbeacbb08c65d6360a5f010a8bd70a9764f9fc
Yechan Jung <skfk286@hanmail.net>
2024-10-01 11:48:55 +0900

    feat : 오름차순, 내림차순 옵션 파라미터 추가

--- src/main/java/org/socialculture/platform/ticket/controller/TicketController.java
+++ src/main/java/org/socialculture/platform/ticket/controller/TicketController.java
@@ -2,9 +2,12 @@ package org.socialculture.platform.ticket.controller;
 
 import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.socialculture.platform.global.apiResponse.ApiResponse;
+import org.socialculture.platform.global.apiResponse.exception.ErrorStatus;
+import org.socialculture.platform.global.apiResponse.exception.GeneralException;
 import org.socialculture.platform.ticket.dto.response.TicketResponseDto;
 import org.socialculture.platform.ticket.service.TicketService;
 import org.springframework.http.ResponseEntity;
@@ -23,9 +26,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/tickets")
 @RequiredArgsConstructor
+@Slf4j
 public class TicketController {
-    private static final Logger logger = LoggerFactory.getLogger(TicketController.class);
-
     private final TicketService ticketService;
 
     /**
@@ -33,13 +35,22 @@ public class TicketController {
      * @param page
      * @param size
      * @param option
+     * @param isAscending
      * @return
      */
     @GetMapping
     public ResponseEntity<ApiResponse<List<TicketResponseDto>>> getAllTicketsByMemberIdWithPageAndSortOptionDesc(
-            @RequestParam("page") int page, @RequestParam("size") int size, @Nullable @RequestParam("option") String option) {
-        logger.info("Get tickets by member id with pagination and sort - Page: {}, Size: {}, Sort Option: {}", page, size, option);
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam(value = "option", defaultValue = "ticketId") String option,
+            @RequestParam(value = "isAscending", defaultValue = "false") boolean isAscending) {
+        log.info("Get tickets by member id with pagination and sort - Page: {}, Size: {}, Sort Option: {}, Ascending : {}", page, size, option, isAscending);
 
-        return ApiResponse.onSuccess(ticketService.getAllTicketsByEmailWithPageAndSortOptionDesc(page, size, option));
+        // page와 size 값이 음수일 경우 예외 처리
+        if (page < 0 || size < 0) {
+            throw new GeneralException(ErrorStatus._TICKET_INVALID_PAGINATION_PARAMETERS);
+        }
+
+        return ApiResponse.onSuccess(ticketService.getAllTicketsByEmailWithPageAndSortOption(page, size, option, isAscending));
     }
 }
