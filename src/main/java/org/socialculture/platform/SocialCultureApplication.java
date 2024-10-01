365c91a1b4d6a1f819ede28f3dc477592c0441c4
Yechan Jung <skfk286@hanmail.net>
2024-10-01 14:41:28 +0900

    refactor : 전체 조회 병합, 상세 조회 코드 병합

--- src/main/java/org/socialculture/platform/SocialCultureApplication.java
+++ src/main/java/org/socialculture/platform/SocialCultureApplication.java
@@ -2,9 +2,10 @@ package org.socialculture.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableJpaAuditing // JPA Auditing 활성화
 public class SocialCultureApplication {
 
