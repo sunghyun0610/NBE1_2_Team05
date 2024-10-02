6aa0ea1b2a638e74b1ca142945711db28fd095bc
icecoff22 <icecoff2222@gmail.com>
2024-10-02 12:42:16 +0900

    test : 공연 등록 테스트 작성

--- src/test/java/org/socialculture/platform/performance/controller/PerformanceControllerTest.java
+++ src/test/java/org/socialculture/platform/performance/controller/PerformanceControllerTest.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.socialculture.platform.performance.dto.domain.CategoryContent;
+import org.socialculture.platform.performance.dto.CategoryDto;
+import org.socialculture.platform.performance.dto.request.PerformanceRegisterRequest;
 import org.socialculture.platform.performance.dto.request.PerformanceUpdateRequest;
 import org.socialculture.platform.performance.dto.response.PerformanceDetailResponse;
 import org.socialculture.platform.performance.dto.response.PerformanceListResponse;
+import org.socialculture.platform.performance.dto.response.PerformanceRegisterResponse;
 import org.socialculture.platform.performance.dto.response.PerformanceUpdateResponse;
 import org.socialculture.platform.performance.entity.PerformanceStatus;
 import org.socialculture.platform.performance.service.PerformanceService;
@@ -34,8 +36,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(PerformanceController.class)
@@ -51,6 +52,104 @@ public class PerformanceControllerTest {
 
     @MockBean
     private PerformanceService performanceService;
+
+    @Test
+    @DisplayName("공연 등록 조회 테스트")
+    @WithMockUser
+    void getPerformanceRegisterTest() throws Exception {
+        //given
+        PerformanceRegisterRequest performanceRegisterRequest =
+                PerformanceRegisterRequest.builder()
+                        .title("제목111")
+                        .dateStartTime(LocalDateTime.now())
+                        .dateEndTime(LocalDateTime.now().plusHours(4L))
+                        .address("주소111111")
+                        .imageUrl("이미지 주소")
+                        .price(10000)
+                        .description("설명입니다.")
+                        .maxAudience(50)
+                        .startDate(LocalDateTime.now().minusDays(2))
+                        .categories(List.of("노래", "스탠드업"))
+                        .build();
+
+        PerformanceRegisterResponse performanceRegisterResponse =
+                PerformanceRegisterResponse.builder()
+                        .memberName("홍길동")
+                        .performanceId(1L)
+                        .title("제목111")
+                        .dateStartTime(LocalDateTime.now())
+                        .dateEndTime(LocalDateTime.now().plusHours(4L))
+                        .description("설명입니다.")
+                        .maxAudience(50)
+                        .address("주소111111")
+                        .imageUrl("이미지 주소")
+                        .price(10000)
+                        .remainingTickets(50)
+                        .startDate(LocalDateTime.now().minusDays(2))
+                        .status(PerformanceStatus.NOT_CONFIRMED)
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .categories(
+                                List.of(
+                                        CategoryDto.of(1L, "노래", "MUSIC"),
+                                        CategoryDto.of(4L, "스탠드업", "STANDUP")
+                                )
+                        )
+                        .build();
+
+        given(performanceService.registerPerformance(performanceRegisterRequest))
+                .willReturn(performanceRegisterResponse);
+
+        ResultActions result = this.mockMvc.perform(
+                post("/api/v1/performances")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(performanceRegisterRequest))
+        );
+
+        result.andExpect(status().isOk())
+                .andDo(document("performance-register",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestFields(
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("공연 제목"),
+                                fieldWithPath("dateStartTime").type(JsonFieldType.STRING).description("공연 시작시간"),
+                                fieldWithPath("dateEndTime").type(JsonFieldType.STRING).description("공연 종료시간"),
+                                fieldWithPath("address").type(JsonFieldType.STRING).description("공연장 주소"),
+                                fieldWithPath("imageUrl").type(JsonFieldType.STRING).description("공연 이미지 URL"),
+                                fieldWithPath("price").type(JsonFieldType.NUMBER).description("티켓 가격"),
+                                fieldWithPath("description").type(JsonFieldType.STRING).description("공연 상세설명"),
+                                fieldWithPath("maxAudience").type(JsonFieldType.NUMBER).description("공연 총 좌석수"),
+                                fieldWithPath("startDate").type(JsonFieldType.STRING).description("판매 시작일"),
+                                fieldWithPath("categories").type(JsonFieldType.ARRAY).description("공연 카테고리 배열(한국어만)")
+                        ),
+                        responseFields(
+                                fieldWithPath("isSuccess").description("성공 여부"),
+                                fieldWithPath("code").description("응답 코드"),
+                                fieldWithPath("message").description("응답 메시지"),
+                                fieldWithPath("httpStatus").description("HTTP 상태"),
+                                fieldWithPath("result.memberName").type(JsonFieldType.STRING).description("주최자 이름"),
+                                fieldWithPath("result.performanceId").type(JsonFieldType.NUMBER).description("공연 ID"),
+                                fieldWithPath("result.title").type(JsonFieldType.STRING).description("공연 제목"),
+                                fieldWithPath("result.dateStartTime").type(JsonFieldType.STRING).description("공연 시작시간"),
+                                fieldWithPath("result.dateEndTime").type(JsonFieldType.STRING).description("공연 종료시간"),
+                                fieldWithPath("result.description").type(JsonFieldType.STRING).description("공연 상세설명"),
+                                fieldWithPath("result.maxAudience").type(JsonFieldType.NUMBER).description("공연 총 좌석수"),
+                                fieldWithPath("result.address").type(JsonFieldType.STRING).description("공연장 주소"),
+                                fieldWithPath("result.imageUrl").type(JsonFieldType.STRING).description("공연 이미지 URL"),
+                                fieldWithPath("result.price").type(JsonFieldType.NUMBER).description("티켓 가격"),
+                                fieldWithPath("result.remainingTickets").type(JsonFieldType.NUMBER).description("남은 티켓 갯수"),
+                                fieldWithPath("result.startDate").type(JsonFieldType.STRING).description("판매 시작일"),
+                                fieldWithPath("result.status").type(JsonFieldType.STRING).description("공연 상태"),
+                                fieldWithPath("result.createdAt").type(JsonFieldType.STRING).description("게시글 생성 시간"),
+                                fieldWithPath("result.updatedAt").type(JsonFieldType.STRING).description("게시글 최종 수정 시간"),
+                                fieldWithPath("result.categories[]").description("공연 카테고리 배열"),
+                                fieldWithPath("result.categories[].categoryId").type(JsonFieldType.NUMBER).description("카테고리 ID"),
+                                fieldWithPath("result.categories[].nameKr").type(JsonFieldType.STRING).description("카테고리 한국어"),
+                                fieldWithPath("result.categories[].nameEn").type(JsonFieldType.STRING).description("카테고리 영어")
+                        )
+                ));
+    }
 
     @Test
     @DisplayName("공연 전체 조회 테스트")
@@ -70,8 +169,8 @@ public class PerformanceControllerTest {
                         .status("NOT_CONFIRMED")
                         .categories(
                                 List.of(
-                                        CategoryContent.of(1L, "노래", "MUSIC"),
-                                        CategoryContent.of(4L, "스탠드업", "STANDUP")
+                                        CategoryDto.of(1L, "노래", "MUSIC"),
+                                        CategoryDto.of(4L, "스탠드업", "STANDUP")
                                 )
                         )
                         .build()
@@ -88,8 +187,8 @@ public class PerformanceControllerTest {
                         .status("CONFIRMED")
                         .categories(
                                 List.of(
-                                        CategoryContent.of(1L, "노래", "MUSIC"),
-                                        CategoryContent.of(2L, "춤", "DANCE")
+                                        CategoryDto.of(1L, "노래", "MUSIC"),
+                                        CategoryDto.of(2L, "춤", "DANCE")
                                 )
                         )
                         .build()
@@ -159,8 +258,8 @@ public class PerformanceControllerTest {
                         .updatedAt(LocalDateTime.now())
                         .categories(
                                 List.of(
-                                        CategoryContent.of(1L, "노래", "MUSIC"),
-                                        CategoryContent.of(4L, "스탠드업", "STANDUP")
+                                        CategoryDto.of(1L, "노래", "MUSIC"),
+                                        CategoryDto.of(4L, "스탠드업", "STANDUP")
                                 )
                         )
                         .build();
