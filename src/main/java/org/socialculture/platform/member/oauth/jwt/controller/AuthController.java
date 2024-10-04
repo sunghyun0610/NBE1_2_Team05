d79b7dc2fbff3201e0b88dd42396ef0113d9eef0
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-10-04 21:38:24 +0900

    feat: 리프레시 토큰 유효성 검사 구현, RefreshTokenEntity 생성

--- src/main/java/org/socialculture/platform/member/oauth/jwt/controller/AuthController.java
+++ src/main/java/org/socialculture/platform/member/oauth/jwt/controller/AuthController.java
@@ -1,9 +1,11 @@
 package org.socialculture.platform.member.oauth.jwt.controller;
 
 import org.socialculture.platform.member.dto.LoginDTO;
+import org.socialculture.platform.member.oauth.jwt.dto.TokenRequestDTO;
 import org.socialculture.platform.member.oauth.jwt.dto.TokenResponseDTO;
 import org.socialculture.platform.member.oauth.jwt.JwtFilter;
 import org.socialculture.platform.member.oauth.jwt.JwtTokenProvider;
+import org.socialculture.platform.member.oauth.jwt.service.AuthService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +20,12 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final JwtTokenProvider jwtTokenProvider;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final AuthService authService;
 
-    public AuthController(JwtTokenProvider jwtTokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
+    public AuthController(JwtTokenProvider jwtTokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder, AuthService authService) {
         this.jwtTokenProvider = jwtTokenProvider;
         this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.authService = authService;
     }
 
     /**
@@ -54,5 +58,31 @@ public class AuthController {
     @PostMapping("/check")
     public void checkToken() {
         System.out.println("토큰 체크");
+    }
+
+
+    /**
+     * 리프레시 토큰 유효성 검사
+     * @param tokenRequestDTO
+     * @return accessToken, refreshToken, message
+     */
+    @GetMapping("/validate")
+    public ResponseEntity<TokenResponseDTO> validateRefreshToken(@RequestBody TokenRequestDTO tokenRequestDTO) {
+        String refreshToken = tokenRequestDTO.getRefreshToken();
+        System.out.println(authService.validateRefreshToken(refreshToken));
+        String message;
+
+        if (!authService.validateRefreshToken(refreshToken)) { //리프레시 토큰 유효하지 않을 때
+            message = "리프레시 토큰이 유효하지 않습니다. 다시 로그인해주세요.";
+            return new ResponseEntity<>(new TokenResponseDTO(message), HttpStatus.OK);
+        }
+
+        String newAccessToken = authService.createNewAccessToken(refreshToken);
+        System.out.println("newAccessToken >>>" + newAccessToken);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer" + newAccessToken);
+
+        message = "새로운 액세스 토큰이 정상적으로 발급되었습니다.";
+        return new ResponseEntity<>(new TokenResponseDTO(newAccessToken, refreshToken, message), httpHeaders, HttpStatus.OK);
     }
 }
