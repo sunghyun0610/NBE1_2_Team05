85c5be425f66e393ea9e35b63b38d7c41bfc6753
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-10-06 20:14:04 +0900

    feat: SecurityConfig jwt 관련 설정 추가

--- src/main/java/org/socialculture/platform/config/SecurityConfig.java
+++ src/main/java/org/socialculture/platform/config/SecurityConfig.java
@@ -1,31 +1,56 @@
 package org.socialculture.platform.config;
 
+import lombok.RequiredArgsConstructor;
+import org.socialculture.platform.member.oauth.jwt.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.SecurityContextHolderFilter;
 
 @EnableWebSecurity
 @Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/members/naver").permitAll()
                         .requestMatchers("/api/v1/members/kakao").permitAll()
+                        .requestMatchers("/api/v1/members/check").hasRole("USER") //2개 role 필요하면 hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/api/v1/members/authenticate").permitAll()
+                        .requestMatchers("/api/v1/members/validate").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
+                .with(new JwtSecurityConfig(jwtTokenProvider), Customizer.withDefaults()) //jwt 보안 설정
                 .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(new JwtFilter(jwtTokenProvider), SecurityContextHolderFilter.class)
                 .build();
     }
 }
