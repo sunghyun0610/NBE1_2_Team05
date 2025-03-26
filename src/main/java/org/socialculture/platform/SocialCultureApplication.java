420bb35f05505565c778b31b0e45b61f9f1ac39d
sunghyun <20190171@inu.ac.kr>
2025-03-26 23:16:39 +0900

    chore : 새로운 db연결 및 env파일

--- src/main/java/org/socialculture/platform/SocialCultureApplication.java
+++ src/main/java/org/socialculture/platform/SocialCultureApplication.java
@@ -1,13 +1,27 @@
 package org.socialculture.platform;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class SocialCultureApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SocialCultureApplication.class, args);
+
+
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        Map<String, Object> env = new HashMap<>();
+        dotenv.entries().forEach(entry -> env.put(entry.getKey(), entry.getValue()));
+
+        SpringApplication app = new SpringApplication(SocialCultureApplication.class);
+        app.setDefaultProperties(env); // 여기에 반드시 주입해야 함
+        app.run(args);
+
+        ;
     }
 
 }
